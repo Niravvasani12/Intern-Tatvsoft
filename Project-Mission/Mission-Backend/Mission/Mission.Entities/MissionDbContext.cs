36dfba8cac0d29943259396e3fa0743b03a780aa
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2025-07-11 08:23:23 +0530

    feat: Mission-Backend

---
+++ Project-Mission/Mission-Backend/Mission/Mission.Entities/MissionDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Models;
+
+namespace Mission.Entities
+{
+    public class MissionDbContext(DbContextOptions<MissionDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Admin",
+                LastName = "Tatva",
+                EmailAddress = "admin@tatvasoft.com",
+                Password = "admin",
+                PhoneNumber = "01234567890",
+                UserType = "admin",
+            },
+            new User()
+            {
+                Id = 2,
+                FirstName = "Nirav",
+                LastName = "Vasani",
+                EmailAddress = "nirav@vasani.com",
+                Password = "nirav@123",
+                PhoneNumber = "9909656231",
+                UserType = "user",
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
