153d9a925c28089e3716d1bac8965413eaefd781
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2025-07-11 08:29:29 +0530

    feat: Mission-Backend

---
+++ Intro-.NET/Mission/README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# Intern-Tatvsoft
+=======
+# Mission
+>>>>>>> 4f75bf5 (Add README.md.)
---
+++ Project-Mission/Mission-Backend/Mission/Mission.Services/IService/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Mission.Entities.ViewModels;
+using Mission.Entities.ViewModels.Login;
+using Mission.Services.IService;
+
+namespace Mission.Services.IService
+{
+    public interface IUserService
+    {
+        Task<ResponseResult> LogiUser(UserLoginRequestModel model);
+    }
+}
